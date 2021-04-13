0356d84936483eac14868af7f1036345958c4340
MuFeng5557 <breezelo2000@gmail.com>
2021-04-13 23:23:22 +0800

    V1.0.5
    Implemented game-menu transmission.

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
-    [SerializeField] private Animator anim;
+    [SerializeField] private Animator menuAnim;
+    [SerializeField] private Animator intoGameAnim;
+    [SerializeField] private string intoGameAnimationName = "Menu-to-game-menu";
 
     public void setOptionTrigger()
     {
-        if(anim!=null) anim.SetTrigger("IntoOption");
+        if(menuAnim != null) menuAnim.SetTrigger("IntoOption");
     }
 
     public void backToMainMenu()
     {
-        if (anim != null) anim.SetTrigger("OutOption");
+        if (menuAnim != null) menuAnim.SetTrigger("OutOption");
+    }
+
+    public void startingGame()
+    {
+        if (intoGameAnim != null) intoGameAnim.SetTrigger("IntoGame");
+        StartCoroutine("loadGame");
+    }
+
+    IEnumerator loadGame()
+    {
+        yield return null;
+        AnimatorStateInfo animInfo = intoGameAnim.GetCurrentAnimatorStateInfo(0);
+        if ((animInfo.IsName(intoGameAnimationName) && animInfo.normalizedTime > 1.0f)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            StartCoroutine("loadGame");
+        }
+        
     }
 }
